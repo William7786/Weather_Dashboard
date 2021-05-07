73b84cde0a053f74dd8ddd65bf96d734a1be92e2
Tcomp <williamt7786@gmail.com>
2021-05-07 03:10:13 -0400

    this commit is for my working api key and weather function

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,26 @@
-$( ".button" ).click(function() {
+
+
+var cities = [];
+var key = "c9d4fc2e822514ab1be527c50cbfeeb2"
+ 
+  $( ".button" ).click(function() { 
     Weather()
   });
+  $("card .stored").val(localStorage.getItem("day"));
+
+  
+  
+
+
 
   function Weather() {
     console.log("This is logging")
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="; 
+    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"; 
+    $("#today-weather").empty();
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+      
+    }) 
+    console.log("logged")
     }
