e5f1b81e2f3eb5b6a408ae7d60284221c2d4b00b
Tcomp <williamt7786@gmail.com>
2021-05-07 17:12:13 -0400

    This is for new functions, trying to fix 401 error

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
-
-
+var cityList =$("#city-list");
 var cities = [];
-var key = "c9d4fc2e822514ab1be527c50cbfeeb2"
+var key = "c9d4fc2e822514ab1be527c50cbfeeb2";
+
  
   $( ".button" ).click(function() { 
     Weather()
@@ -9,13 +9,36 @@ var key = "c9d4fc2e822514ab1be527c50cbfeeb2"
   $("card .stored").val(localStorage.getItem("day"));
 
   
-  
+  function FormatDay(date){
+    var date = new Date();
+    console.log(date);
+    var month = date.getMonth()+1;
+    var day = date.getDate();
+    
+    var dayOutput = date.getFullYear() + '/' +
+        (month<10 ? '0' : '') + month + '/' +
+        (day<10 ? '0' : '') + day;
+    return dayOutput;
+}
+stored()
+function stored(){
+var storedCity = JSON.parse(localStorage.getItem("cities"))
+if (storedCity !== null){
+cities = storedCity;
+}
+  }
+
+function saveCity(){
+localStorage.setItem("cities", JSON.stringify(cities))
+console.log(savedCity);
 
 
+}
 
-  function Weather() {
+
+function Weather(cityName) {
     console.log("This is logging")
-    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"; 
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +cityName+ "&appid=" + key;
     $("#today-weather").empty();
     $.ajax({
       url: queryURL,
@@ -23,4 +46,4 @@ var key = "c9d4fc2e822514ab1be527c50cbfeeb2"
       
     }) 
     console.log("logged")
-    }
+     };
